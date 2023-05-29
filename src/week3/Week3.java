0cfbd9270499b94017f4643ffc84c4f646e23253
patdixan <133692235+patdixan@users.noreply.github.com>
2023-05-29 10:50:28 -0700

    Update Week3.java

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -16,6 +16,7 @@ public class Week3 {
     public static void main(String[] args) {
         System.out.println("Lerning github operation");
         System.out.println("#2 Lerning github operation");
+         System.out.println("#3 Lerning github operation");
     }
     
 }
